ba46eeecd5776f14f6bd4de23ab84b064f33ef14
Kristian Keller <kristian.keller@studenti.unimi.it>
2022-12-28 13:41:33 +0100

    Initial Commit

---
+++ Countries Iliad/Countries_IliadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countries_IliadApp.swift
+//  Countries Iliad
+//
+//  Created by Kristian Keller on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Countries_IliadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
